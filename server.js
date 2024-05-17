5bb16af0ae026218833b786278f78adc15e3c514
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2024-05-17 13:46:44 +0530

    Redirection Page & Already Shortened URL Fixed

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ app.get('/api/shortUrls/all', async (req, res) => {
 app.post('/api/shortUrls', async (req, res) => {
     try {
         const { fullUrl, creator } = req.body;
+
+        // Check if the provided URL has already been shortened
+        const existingShortUrl = await ShortURL.findOne({ full: fullUrl });
+        if (existingShortUrl) {
+            console.log('URL already shortened:', existingShortUrl);
+            return res.status(200).json(existingShortUrl);
+        }
+
+        // If the URL hasn't been shortened before, create a new short URL
         const newShortUrl = await ShortURL.create({ full: fullUrl, creator: creator });
         console.log('Short URL created:', newShortUrl);
         res.status(201).json(newShortUrl);
@@ -69,12 +78,66 @@ app.get('/:shortUrl', async (req, res) => {
         shortUrl.clicks++;
         await shortUrl.save();
         console.log('Redirecting to:', shortUrl.full);
-        res.redirect(shortUrl.full);
+
+        res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Redirecting...</title>
+            <meta http-equiv="refresh" content="3; URL='${shortUrl.full}'" />
+            <link href="https://rsms.me/inter/inter.css" rel="stylesheet">
+            <style>
+                html{
+                    font-feature-settings: 'cv11';
+                }
+                body {
+                    font-family: 'Inter', sans-serif;
+                    padding:15px;
+                }
+                .container{
+                    width:100%;
+                    height:calc(100vh - 50px);
+                    display:flex;
+                    flex-direction:column;
+                    justify-content:center;
+                    align-items:Center;
+                    text-align:center;
+                }
+                .button{
+                    background-color: black;
+                    color:white;
+                    font-weight:500;
+                    padding-inline:1rem;
+                    padding-block:0.8rem;
+                    border-radius:0.6rem;
+                    cursor:pointer;
+                    transition:all ease-in-out 0.3s;
+                    text-decoration:none;
+                }
+                .button:hover{
+                    scale:1.05;
+                    background: #797979;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <p>Redirecting to ${shortUrl.full}...</p>
+                <a class="button" href="https://tinypath.netlify.app/" target="_blank">Shorten URL</a>
+            </div>
+        </body>
+        </html>
+        
+        `);
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
